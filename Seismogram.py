5202a36c924bfb7db81a7cbddccd5b7851898dd1
nienkebrinkman <nienkebrinkman@hotmail.com>
2018-04-26 13:45:40 +0200

    Seismogram based on all moment components (Not based on strike, rake, dip any more)

--- Seismogram.py
+++ Seismogram.py
@@ -8,13 +8,16 @@ class Seismogram:
         self.db = db
         self.receiver = instaseis.Receiver(latitude=self.par['la_r'], longitude=self.par['lo_r'],
                                            network=self.par['network'], station=self.par['station'])
-        self.source = instaseis.Source.from_strike_dip_rake(latitude=self.par['la_s'], longitude=self.par['lo_s'],
-                                                            depth_in_m=self.par['depth_s'],
-                                                            strike=self.par['strike'], dip=self.par['dip'],
-                                                            rake=self.par['rake'], M0=self.par['M0'])
+        # self.source = instaseis.Source.from_strike_dip_rake(latitude=self.par['la_s'], longitude=self.par['lo_s'],
+        #                                                     depth_in_m=self.par['depth_s'],
+        #                                                     strike=self.par['strike'], dip=self.par['dip'],
+        #                                                     rake=self.par['rake'], M0=self.par['M0'])
+        self.source = instaseis.Source(latitude=self.par['la_s'], longitude=self.par['lo_s'],
+                                       depth_in_m=self.par['depth_s'], m_tp=self.par['m_tp'], m_rp=self.par['m_rp'],
+                                       m_rt=self.par['m_rt'], m_pp=self.par['m_pp'], m_tt=self.par['m_tt'],
+                                       m_rr=self.par['m_rr'])
 
     def get(self):
-
         traces = self.db.get_seismograms(source=self.source, receiver=self.receiver, components=self.par['components'],
                                          kind=self.par['kind'])
         seismogram = np.array([])
